2ecae1c4d6e6a4eeac6a4c049835e47dbadbbdb9
Alessandro Gri <alessandro.gri.ag@gmail.com>
2025-05-04 15:59:10 +0200

    Update event handler class names and bump version to 0.1.4

--- @AZM_Miclic/addons/azm_miclic/CfgEventHandlers.hpp
+++ @AZM_Miclic/addons/azm_miclic/CfgEventHandlers.hpp
@@ -1,11 +1,11 @@
 class Extended_PreInit_EventHandlers {
-    class azm_bft {
+    class azm_miclic {
         init = "call compileScript ['\azm_miclic\XEH_preInit.sqf']";
     };
 };
 
 class Extended_PostInit_EventHandlers {
-    class azm_bft {
+    class azm_miclic {
         init = "call compileScript ['\azm_miclic\XEH_postInit.sqf']";
     };
 };
